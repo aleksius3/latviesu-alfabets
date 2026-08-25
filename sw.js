1f7baedd9450aaa05473fcd7196de4e8d037321f
aleksius3 <122536752+aleksius3@users.noreply.github.com>
2026-08-25 22:41:22 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,35 @@
-const CACHE = "latviesu-abc-v2";
-const CORE = [
-  "./","./index.html","./style.css","./app.js","./manifest.webmanifest",
-  "./icons/icon-192.png","./icons/icon-512.png"
-];
+const CACHE = "latviesu-abc-runtime";
 
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then(cached =>
-      cached || fetch(event.request).then(response => {
+    fetch(event.request)
+      .then(response => {
         const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+
+        caches.open(CACHE).then(cache => {
+          cache.put(event.request, copy);
+        });
+
         return response;
-      }).catch(() => caches.match("./index.html"))
-    )
+      })
+      .catch(() => caches.match(event.request))
   );
 });
