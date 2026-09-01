9f400a99f9d1c310417dcd4f840cbeb35aaee429
aleksius3 <122536752+aleksius3@users.noreply.github.com>
2026-09-01 22:16:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,3 @@
-const CACHE = "latviesu-abc-v6";
-
 self.addEventListener("install", () => {
   self.skipWaiting();
 });
@@ -7,7 +5,7 @@ self.addEventListener("install", () => {
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
+      Promise.all(keys.map(key => caches.delete(key)))
     ).then(() => self.clients.claim())
   );
 });
@@ -16,12 +14,6 @@ self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
 
   event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
-        caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        return response;
-      })
-      .catch(() => caches.match(event.request))
+    fetch(event.request, { cache: "no-store" })
   );
 });
