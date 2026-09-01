4215b745f9ed6b312f7a645368a3029566548465
aleksius3 <122536752+aleksius3@users.noreply.github.com>
2026-09-01 22:10:42 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,27 @@
-const CACHE="latviesu-abc-runtime-v5";
-self.addEventListener("install",()=>self.skipWaiting());
-self.addEventListener("activate",event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+const CACHE = "latviesu-abc-v6";
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
-self.addEventListener("fetch",event=>{
-  if(event.request.method!=="GET")return;
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    fetch(event.request).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-      return response;
-    }).catch(()=>caches.match(event.request))
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
